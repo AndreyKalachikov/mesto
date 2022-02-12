1e021d8f8cd0af5f15abbd0c99c44ad49d9e960a
Andrey Kalachikov <gaure@yandex.ru>
2022-02-12 15:37:37 +0300

    fix all  bugs

--- scripts/index.js
+++ scripts/index.js
@@ -1,44 +1,43 @@
 const profileEditButton = document.querySelector('.profile__edit-button');
 const popupElement = document.querySelector('.popup');
 const popupCloseButtonElement = popupElement.querySelector('.popup__close-button');
-
-const addPopup = function() {
-  popupElement.classList.add("popup_opened");
-}
-const removePopup = function() {
-  popupElement.classList.remove("popup_opened");
-}
-
-profileEditButton.addEventListener('click', addPopup);
-popupCloseButtonElement.addEventListener('click', removePopup);
-
-
-
-
 let formElement = document.querySelector('.popup__form');
 let nameInput = formElement.querySelector('#nameInput');
 let jobInput = formElement.querySelector('#jobInput');
+let profileName = document.querySelector('.profile__name');
+let profilePersonal = document.querySelector('.profile__personal');
 
+// Функции открытия и закрытия попапа
+const addPopup = function() {
+  popupElement.classList.add('popup_opened');
+  nameInput.value = profileName.textContent;
+  jobInput.value = profilePersonal.textContent;
+}
+const removePopup = function() {
+  popupElement.classList.remove('popup_opened');
+}
+
+// Функция сохранения внесенных в профиль изменений
 function formSubmitHandler (evt) {
   evt.preventDefault()
-
-  let profileName = document.querySelector('.profile__name');
-  let profilePersonal = document.querySelector('.profile__personal');
-
   profileName.textContent = nameInput.value;
   profilePersonal.textContent = jobInput.value;
-
 removePopup()
 }
 
+
+profileEditButton.addEventListener('click', addPopup);
+popupCloseButtonElement.addEventListener('click', removePopup);
 formElement.addEventListener('submit', formSubmitHandler);
 
 
 
 
 
-
-
-
-
-
+// Ориентир кнопки лайка для следующей проектной работы
+// let likeButtons = document.querySelectorAll('.elements__like-button');
+// for(let i = 0; i < likeButtons.length ; i++) {
+//   likeButtons[i].addEventListener('click', function () {
+//     likeButtons[i].classList.toggle('elements__like-button_pressed');
+//   });
+// }
